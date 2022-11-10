0c837b24d87c6d830ffb74cbe177795564cbad8f
nourinfathima <nourinnourin36@gmail.com>
2022-11-10 11:21:43 +0530

    firstcommit

--- admin.py
+++ admin.py
@@ -1,3 +1,9 @@
+
+import mysql.connector
+
+mydb = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'ksebdb')
+
+mycursor = mydb.cursor()
 while True:
 
     print("select an option from menu")
