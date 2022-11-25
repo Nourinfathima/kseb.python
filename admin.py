4d89c95fc27d1ce76d3302b1db5b70430c192636
nourin <nourin@NTP-LAP-636.chn.nestgroup.net>
2022-11-25 10:20:23 +0530

    second

--- admin.py
+++ admin.py
@@ -1,5 +1,7 @@
 
 import mysql.connector
+from datetime import date
+from tabulate import tabulate
 
 mydb = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'ksebdb')
 
@@ -31,7 +33,7 @@ while True:
         consumerName = input("Enter the consumer name: ")
         consumerPhone = input("Enter the consumer phone: ")
         consumerAddress = input("Enter the consumer address: ")
-        sql = "INSERT INTO `consumer`(`consumerCode`, `consumerName`, `consumerPhone`, `consumerAddress`) VALUES (%s,%s,%s,%s,%s)"
+        sql = "INSERT INTO `consumer`(`consumerCode`, `consumerName`, `consumerPhone`, `consumerAddress`) VALUES (%s,%s,%s,%s)"
         data = (consumerCode,consumerName,consumerPhone,consumerAddress)
         mycursor.execute(sql,data)
         mydb.commit()
@@ -41,7 +43,7 @@ while True:
 
         print("Search Consumer selected")
         searchOption = input("Enter the Consumer Code/Name/Phone to search: ")
-        sql = "SELECT `consumerCode`, `consumerName`, `consumerPhone`8, `consumerAddress` FROM `consumer` WHERE `consumerCode` ='"+searchOption+"'  OR `consumerName`='"+searchOption+"' OR `consumerPhone` ='"+searchOption+"' "
+        sql = "SELECT `consumerCode`, `consumerName`, `consumerPhone`, `consumerAddress` FROM `consumer` WHERE `consumerCode` ='"+searchOption+"'  OR `consumerName`='"+searchOption+"' OR `consumerPhone` ='"+searchOption+"' "
         mycursor.execute(sql)
         result = mycursor.fetchall()
         for i in result:
@@ -58,18 +60,60 @@ while True:
 
     elif(choice==4):
 
-        print("update employe selected")
+        
+        print("Update Consumer selected")
+        consumerCode = input("Enter the consumer code to update consumer: ")
+        consumerName = input("Enter the consumer name to update: ")
+        consumerPhone = input("Enter the consumer phone to update: ")
+        consumerEmail = input("Enter the consumer email id to update: ")
+        consumerAddress = input("Enter the consumer address to update: ")
+        sql = "UPDATE `consumer` SET `consumerName`='"+consumerName+"',`consumerPhone`='"+consumerPhone+"',`consumerAddress`='"+consumerAddress+"' WHERE `consumerCode` = "+consumerCode
+        mycursor.execute(sql)
+        mydb.commit()
+        print(" Consumer Data updated successfully")
 
     elif(choice==5):
-        print("view employe selected")
+       
+       print("View All Consumer selected")
+       sql = "SELECT `consumerCode`, `consumerName`, `consumerPhone`, `consumerAddress` FROM `consumer` "
+       mycursor.execute(sql)
+       result = mycursor.fetchall()
+       for i in result:
+            print(i)
+       
 
     elif(choice==6):
-        print("generate the consumerbill ")
+        print('generate bill selected')
+        dates = date.today()
+        year = dates.year
+        month = dates.month
+        sql="DELETE FROM `bill` WHERE `month`='"+str(month)+"' AND `year`= '"+str(year)+"'"
+        mycursor.execute(sql)
+        mydb.commit()
 
-    elif(choice==7):
-        print("view bill")
-
+        sql="SELECT `id` FROM `bill`"
+        mycursor.execute(sql)
+        result=mycursor.fetchall()
+        for i in result:
+            a=i[0]
+            print(a)
+            
+            sql="SELECT SUM(unit) FROM `usage` WHERE `consumercode`='"+str(a)+"' AND MONTH(datetime)='"+str(month)+"' AND YEAR(datetime)='"+str(year)+"' "
+            mycursor.execute(sql)
+            result=mycursor.fetchone()
+            unit=(result[0])
+            print(result)
+            #print(i)
+            total_bill=int(str(result[0])) * 5
+            print(total_bill)
+            #date= datetime.today().strftime('%Y-%m-%d')
+            sql="INSERT INTO `bill`(`consumercode`, `month`, `year`, `bill`, `billdate`, `billstatus`, `totalunit`) VALUES (%s,%s,%s,%s,%s,now(),%s,now()+interval 14 day)"
+            data = (str(a),str(month),str(year),total_bill,'0',unit)
+            mycursor.execute(sql,data)
+            mydb.commit()
+            print("Bill inserted successfully.")
+        
     elif(choice==8):
-        print("Exit")
-
-        break
+      print("Exit")
+    break
+ 
